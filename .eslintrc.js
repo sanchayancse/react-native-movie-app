3084bfc85b9591d849394b743e3df6b9459e9f6f
sanchayancse <sanchayandas1996@gmail.com>
2023-09-02 20:36:03 +0530

    code update for bottom navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  plugins: ['react', 'simple-import-sort'],
+  rules: {
+    // increase the severity of rules so they are auto-fixable
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  'sort-imports': [
+    'error',
+    {
+      ignoreCase: false,
+      ignoreDeclarationSort: false,
+      ignoreMemberSort: false,
+      memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      allowSeparatedGroups: false,
+    },
+  ],
 };
